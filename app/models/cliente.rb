23fdd98b4ba3121f73cb2287a6c3646db90360bb
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-11-14 00:18:33 -0200

    form de cadastro OK tabela de index ok

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,7 @@
 class Cliente < ApplicationRecord
+
+    validates :nome, presence: true
+    validates :tel, presence: true
+    validates :cep, presence: true
+    validates :rua, presence: true
 end
